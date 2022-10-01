95b4ef669d2f0d7e3f6b0f3d1e3e409df95ee1a7
Tim Knops <timknops22@gmail.com>
2022-10-02 01:22:47 +0200

    Adding UI to the rps

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const container = document.querySelector('.results');
+const computerScore = document.querySelector('#comp-score');
+const playerScore = document.querySelector('#play-score');
+
+const results = document.createElement('h1');
+results.classList.add('result');
+container.appendChild(results);
+
+rock.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound('rock', computerSelection);
+});
+
+paper.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound('paper', computerSelection);
+})
+
+scissors.addEventListener('click', () => {
+    const computerSelection = getComputerChoice();
+    playRound('scissors', computerSelection);
+})
+
+let computerStartingScore = 0;
+let playerStartingScore = 0;
+
 function getComputerChoice() {
     choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -5,47 +34,33 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     const returnMessage = [`You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`, `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`];
-
+    
     if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log(returnMessage[0]);
+        results.textContent = returnMessage[0];
+        computerScore.textContent = computerStartingScore++;
         return false;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log(returnMessage[1]);
+        results.textContent = returnMessage[1];
+        playerScore.textContent = playerStartingScore++;
         return true;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log(returnMessage[0]);
+        results.textContent = returnMessage[0];
+        computerScore.textContent = computerStartingScore++;
         return false;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log(returnMessage[1]);
+        results.textContent = returnMessage[1];
+        playerScore.textContent = playerStartingScore++;
         return true;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log(returnMessage[0]);
+        results.textContent = returnMessage[0];
+        computerScore.textContent = computerStartingScore++;
         return false;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log(returnMessage[1]);
+        results.textContent = returnMessage[1];
+        playerScore.textContent = playerStartingScore++;
         return true;
     } else {
-        console.log("It's a Tie!");
+        results.textContent = "It's a Tie!";
         return null;
     }
 }
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-        const computerSelection = getComputerChoice();
-        round = playRound(playerSelection, computerSelection);
-
-        if (round === true) {
-            playerScore++;
-        } else if (round === false) {
-            computerScore++;
-        }
-    }
-    console.log(`Your Score: ${playerScore}\nComputer's Score: ${computerScore}`);
-}
-
-game();
